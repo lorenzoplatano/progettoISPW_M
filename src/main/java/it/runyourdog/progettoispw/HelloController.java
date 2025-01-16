e46515f075380279cdbcc74682d860373d69a6fc
lorenzo <lorenzo.platano@students.uniroma2.eu>
2025-01-16 11:23:42 +0100

    ok

--- src/main/java/it/runyourdog/progettoispw/HelloController.java
+++ src/main/java/it/runyourdog/progettoispw/HelloController.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,7 +20,7 @@ public class HelloController {
     private Button padroneButton;
 
     @FXML
-    private Button registerNowButton;
+    private Text registerNowButton;
 
     @FXML
     public void provaButton(){
